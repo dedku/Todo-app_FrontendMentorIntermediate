0fe706695bb9d68e02e3c16bb4e8101e720deae6
Patryk Szabat <szabat.patryk@gmail.com>
2023-04-29 10:50:32 +0200

    fix: remove app.get('/') && add middleware

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,13 @@
-import express, { Express, Request, Response } from 'express'
+import express, { Express } from 'express'
 import connectDB from './db/connect.ts'
 import tasks from './routes/tasks.ts'
 
 const app: Express = express()
 const port: number = 3000
 
-
-app.get('/', (_: Request, res: Response) => {
-    res.status(200).send("Hellow word")
-})
-
+app.use(express.static('./public'))
 app.use(express.json())
+
 app.use('/api/v1/tasks', tasks)
 
 const start = async () => {
