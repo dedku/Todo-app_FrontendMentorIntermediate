c087ccf75483bfde77f14310c405a4d18ab2dfee
Patryk Szabat <szabat.patryk@gmail.com>
2023-04-29 10:03:23 +0200

    feat: task shema validation update

--- src/models/task.ts
+++ src/models/task.ts
@@ -2,8 +2,17 @@ import mongoose from 'mongoose'
 
 
 const TaskSchema = new mongoose.Schema({
-    name: String,
-    compleated: Boolean
+    name: {
+        type: String,
+        require: [true, 'Task name is missing in provided request'],
+        trim: true,
+        min: [3, 'Task name is too short, please provide longer name, got {VALUE}'],
+        maxLength: [20, 'Task name is too long, please provide shorten name (max 20 characters ), got {VALUE}']
+    },
+    compleated: {
+        type: Boolean,
+        require: true
+    }
 })
 
 export const Task = mongoose.model('Task', TaskSchema)
